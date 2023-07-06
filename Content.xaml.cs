a3914045138f6fd385c0ab0bb8e8defd671b9369
Aravinthan <134296054+Aravinthan-A@users.noreply.github.com>
2023-07-06 11:41:39 +0530

    Add files via upload

---
+++ Content.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace PageScreen.Dairy
+{
+    /// <summary>
+    /// Interaction logic for Content.xaml
+    /// </summary>
+    public partial class Content : Page
+    {
+        public Content()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(@"C:\Users\welcome\Documents\"+txtyou.Text+".txt",txtcontent.Text);
+        }
+    }
+}
---
+++ Create.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using PageScreen.Dairy;
+
+namespace PageScreen.Main
+{
+    /// <summary>
+    /// Interaction logic for Create.xaml
+    /// </summary>
+    public partial class Create : Window
+    {
+       // Name oname;
+       // Content ocontent;
+        public Create()
+        {
+            InitializeComponent();
+           // oname = new Name();
+            Content  content= new Content();
+            frmfirst.Content = content;
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //ocontent = new Content();
+            //frmsecond.Content = ocontent;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            frmfirst.GoBack();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            frmfirst.GoForward();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Content = new Content();
+            frmfirst.Content = Content;
+        }
+    }
+}
